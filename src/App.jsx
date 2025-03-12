68c4139d06672676e2767b2df34adfa137921954
muhammad-junaid1 <mjunaid.swe@gmail.com>
2025-03-12 19:36:42 +0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,31 @@
-import { Navigate , Routes, Route } from "react-router-dom";
-import HomePage from './pages/home/HomePage';
-import BookmarksPage from './pages/bookmark';
-import SignUpPage from './pages/auth/signup/SignUpPage';
-import LoginPage from './pages/auth/login/LoginPage';
-import NotificationPage from './pages/notification/NotificationPage';
-import ProfilePage from './pages/profile/ProfilePage';
+import { Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/home/HomePage";
+import BookmarksPage from "./pages/bookmark";
+import SignUpPage from "./pages/auth/signup/SignUpPage";
+import LoginPage from "./pages/auth/login/LoginPage";
+import NotificationPage from "./pages/notification/NotificationPage";
+import ProfilePage from "./pages/profile/ProfilePage";
 
-import Sidebar from './components/common/Sidebar';
-import RightPanel from './components/common/RightPanel';
+import Sidebar from "./components/common/Sidebar";
+import RightPanel from "./components/common/RightPanel";
 
-import { Toaster } from 'react-hot-toast';
-import { useQuery } from '@tanstack/react-query';
+import { Toaster } from "react-hot-toast";
+import { useQuery } from "@tanstack/react-query";
 import LoadingSpinner from "./components/common/LoadingSpinner";
 import ForgotPasswordPage from "./pages/forgotpassword";
 import ResetPasswordPage from "./pages/resetPassword";
 import SearchPage from "./pages/search";
 
-
 function App() {
-  const { data:authUser, isLoading } = useQuery({
-    queryKey: ['authUser'],
+  const { data: authUser, isLoading } = useQuery({
+    queryKey: ["authUser"],
     queryFn: async () => {
       try {
-        const res = await fetch('/api/auth/me');
+        const res = await fetch("/api/auth/me");
         const data = await res.json();
-        if(data.error) return null;
+        if (data.error) return null;
         if (!res.ok) {
-          throw new Error(data.message || 'Something went wrong');
+          throw new Error(data.message || "Something went wrong");
         }
         console.log("authUser", data);
         return data;
@@ -37,57 +36,58 @@ function App() {
     retry: false,
   });
   if (isLoading) {
-      return (
-        <div className="h-screen flex justify-center items-center">
-          <LoadingSpinner size="lg" />
-        </div>
-      );
-  }
-  return (
-      <div className="flex max-w-6xl mx-auto">
-        {authUser && <Sidebar />}
-        <Routes>
-          <Route
-            path="/"
-            element={authUser ? <HomePage /> : <Navigate to="/login" />}
-          />
-          <Route
-            path="/login"
-            element={!authUser ? <LoginPage /> : <Navigate to="/" />}
-          />
-          <Route
-            path="/forgot-password"
-            element={!authUser ? <ForgotPasswordPage /> : <Navigate to="/" />}
-          />
-          <Route
-            path="/reset-password"
-            element={!authUser ? <ResetPasswordPage /> : <Navigate to="/" />}
-          />
-          <Route
-            path="/signup"
-            element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
-          />
-          <Route
-            path="/notifications"
-            element={authUser ? <NotificationPage /> : <Navigate to="/login" />}
-          />
-          <Route
-            path="/bookmarks"
-            element={authUser ? <BookmarksPage /> : <Navigate to="/login" />}
-          />
-          <Route
-            path="/search"
-            element={authUser ? <SearchPage /> : <Navigate to="/login" />}
-          />
-          <Route
-            path="/profile/:username"
-            element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
-          />
-        </Routes>
-        {authUser?._id && <RightPanel />}
-        <Toaster />
+    return (
+      <div className="h-screen flex justify-center items-center">
+        <LoadingSpinner size="lg" />
       </div>
     );
+  }
+  return (
+    <div className="flex max-w-6xl mx-auto">
+      {/* <Navbar/> */}
+      {authUser && <Sidebar />}
+      <Routes>
+        <Route
+          path="/"
+          element={authUser ? <HomePage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/forgot-password"
+          element={!authUser ? <ForgotPasswordPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/reset-password"
+          element={!authUser ? <ResetPasswordPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/signup"
+          element={!authUser ? <SignUpPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/notifications"
+          element={authUser ? <NotificationPage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/bookmarks"
+          element={authUser ? <BookmarksPage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/search"
+          element={authUser ? <SearchPage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/profile/:username"
+          element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
+        />
+      </Routes>
+      {authUser?._id && <RightPanel />}
+      <Toaster />
+    </div>
+  );
 }
 
-export default App    
+export default App;
